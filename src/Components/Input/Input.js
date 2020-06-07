bd3ba23c52b0ae36c54aa9d387ee858f2f747e61
schmem55 <scemama.jeremy@gmail.com>
2020-06-07 18:13:38 +0300

    delete workfs

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 import './input.css';
-import Members from '../Members/Members'
 
 export default function Input(props) {
   const [members,setMembers] = useState([])
   const [value,setValue] = useState()
-  
+
   async function handleSubmit(e){
     e.preventDefault()
-    await setMembers(prevState=>[...members,value])
+
+
+    let indexMember = members.length +1;
+
+    let newMember = {
+      id:indexMember,
+      name:value
+    }
+
+    console.log(newMember)
+
+    await setMembers(prevState=>[...members,newMember])
     setValue('')
-    
   }
+
+  function deleteMember (id){
+    setMembers(members.filter((member)=>member.id !== id))
+  }
+
   return (
     <div>
       <h1>CRUD EXCERCISE</h1>
@@ -22,7 +36,19 @@ export default function Input(props) {
         </label>
         <input type="submit" value="Add Member" />
       </form>
-      <Members members = {members}/>
+      
+      <div className="Members">
+        {members.length>0?
+          members.map((k)=>
+            <p key={k.id}>
+              {k.name}
+              <button onClick={()=>deleteMember(k.id)}>delete</button>
+            </p>
+        ):
+        <p>no members added</p>}
+      </div>
+     
+     
     </div>
   );
 }
